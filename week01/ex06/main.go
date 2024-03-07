471d1b2fe18cd8b56701b266c3f762754cd6a128
Yugyeong <uugyeong27@gmail.com>
2024-03-07 17:19:30 +0900

    Study: go-training 6 go concurrency

--- week01/ex06/main.go
+++ week01/ex06/main.go
@@ -2,26 +2,14 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
-func cacl(i int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	t := 0
-	for i := 1; i < 1000000; i++ {
-		t++
-	}
-
-	fmt.Println(i, t)
-}
-
 func main() {
-	wg := sync.WaitGroup{}
-	wg.Add(10)
-	for i := 0; i < 10; i++ {
-		go cacl(i, &wg)
-	}
-
-	wg.Wait()
-	fmt.Println("End!!!!")
+	msg := "Let's Go"
+	go func(msg string) {
+		fmt.Println(msg)
+	}(msg)
+	msg = "Let's GoGoGo"
+	time.Sleep(1 * time.Second)
 }
